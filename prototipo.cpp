da70e976acb5cfd08611061deafbc0ae7ca2aa02
Ramom Pereira <rpss@cin.ufpe.br>
2017-09-20 07:07:35 -0300

    Com a discretização e erro

--- prototipo.cpp
+++ prototipo.cpp
@@ -1,11 +1,20 @@
 #include "Aria.h"
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <math.h>
+#include <utility>
 
-const int TAM_MATRIZ = 61;
+using namespace std;
+
+const int TAM_MATRIZ = 15500 / 400;
 const int MATRIZ_METADE = TAM_MATRIZ / 2;
 const int INFINITO = 2147483646;
 
 int matriz[TAM_MATRIZ][TAM_MATRIZ];
+
+int inicial_x, inicial_y, head;
+int final_x, final_y;
 
 class ActionGo: public ArAction {
   public:
@@ -26,6 +35,7 @@ ActionGo::ActionGo(double maxSpeed, ArActionGoto *go_to) :
   mySonar = NULL;
   myMaxSpeed = maxSpeed;
   myGoto = go_to;
+  myDesired.setHeading(head);
 }
 
 void ActionGo::setRobot(ArRobot *robot)
@@ -38,63 +48,96 @@ void ActionGo::setRobot(ArRobot *robot)
       deactivate();
     }
 }
-ArActionDesired *ActionGo::fire(ArActionDesired currentDesired)
-{
-  double range;
-  double speed;
 
+double calc_dist(double x1, double y1, double x2, double y2){
+  return sqrt(((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)));
+}
+
+ArActionDesired *ActionGo::fire(ArActionDesired currentDesired) {
+
+  //coordenadas do robo no mapa
+  double posRoboX = myRobot->getX();
+  double posRoboY = myRobot->getY();
+
+  //posicao na matriz do robo
+  int posMatrizRoboX = posRoboX / 510;
+  int posMatrizRoboY = posRoboY / 510;
+
+  matriz[posMatrizRoboX + MATRIZ_METADE][posMatrizRoboY + MATRIZ_METADE] = -1;
+
+  if(posMatrizRoboX == ((final_x - inicial_x) / 510) && posMatrizRoboY == ((final_y - inicial_y) / 510)) {
+    return &myDesired;
+  }
   myDesired.reset();
 
-    //coordenadas do robo no mapa
-    double posRoboX = myRobot->getX();
-    double posRoboY = myRobot->getY();
+  //priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> heap;
+  priority_queue<pair<double,int>,  vector<pair<double,int>>, greater<pair<double,int>>> heap;
 
-    //posicao na matriz do robo
-    int posMatrizRoboX = posRoboX / 510;
-    int posMatrizRoboY = posRoboY / 510;
-
-    matriz[posMatrizRoboX + MATRIZ_METADE][posMatrizRoboY + MATRIZ_METADE] = -1;
-
-    for(int i = 0; i < myRobot->getNumSonar(); i++) {
+  for(int i = 0; i < myRobot->getNumSonar(); i++) {
       
-      ArSensorReading *sonar = myRobot->getSonarReading(i);
-      int alcance = myRobot->getSonarRange(i);
+    ArSensorReading *sonar = myRobot->getSonarReading(i);
+    int alcance = myRobot->getSonarRange(i);
       
-      //coordenadas do range
-      double cordX = sonar->getX();
-      double cordY = sonar->getY();
+    //coordenadas do range
+    double cordX = sonar->getX();
+    double cordY = sonar->getY();
 
-      //posicao na matriz
-      int posMatrizX = cordX / 510;
-      int posMatrizY = cordY / 510;
+    //posicao na matriz
+    int posMatrizX = cordX / 510;
+    int posMatrizY = cordY / 510;
 
-      //preechendo na matriz os lugares alcançados pelo robo
-      if(alcance < 4990) {
-        matriz[posMatrizX + MATRIZ_METADE][posMatrizY + MATRIZ_METADE] = INFINITO;
-      } else if(matriz[posMatrizX][posMatrizY] != INFINITO) {
-        matriz[posMatrizX + MATRIZ_METADE][posMatrizY + MATRIZ_METADE] = 0;
-      }
+    //preechendo na matriz os lugares alcançados pelo robo
+    if(alcance < 3600) {
+      matriz[posMatrizX + MATRIZ_METADE][posMatrizY + MATRIZ_METADE] = INFINITO;
+    } else if(matriz[posMatrizX + MATRIZ_METADE][posMatrizY + MATRIZ_METADE] != INFINITO) {
+      /*if((matriz[posMatrizX + MATRIZ_METADE][posMatrizY + MATRIZ_METADE] == -1 && i == 15) || 
+        (matriz[posMatrizX + MATRIZ_METADE][posMatrizY + MATRIZ_METADE] == 1)) {
+        matriz[posMatrizX + MATRIZ_METADE][posMatrizY + MATRIZ_METADE] = 0;*/
 
+        int factor = 1, num_it = 3;
+        vector<pair<double,double>> points;
+        for(int j = 0 ; j < num_it ; ++j){
+          double a = (cordX/factor);
+          double b = (cordY/factor);
+          factor *= 2;
+          points.push_back(make_pair(a,b));                
+        }
+        for(int j = 0 ; j < num_it ; ++j){
+          double distance = calc_dist(points[j].first, points[j].second,(final_x - inicial_x), 
+            (final_y - inicial_y));
+          heap.push(make_pair(distance/myRobot->getSonarReading(i)->getRange(), i));
+        }
+      //}
     }
+  }
 
-    myRobot->lock();
-    myGoto->setGoal(ArPose(10000, 10000));
-    myRobot->unlock();
-
-    ArUtil::sleep(100);
-
-  //range = mySonar->currentReadingPolar(-70, 70) - myRobot->getRobotRadius();
-  //myDesired.setVel(range);
+  /*myRobot->lock();
+  myGoto->setGoal(ArPose((400 * myMap.find(heap.top())->second.first), (400 * myMap.find(heap.top())->second.second)));
+  myRobot->unlock();
+*/
+  myDesired.setHeading(myRobot->getSonarReading(heap.top().second)->getSensorTh());
+  myDesired.setVel(300);
+  ArUtil::sleep(100);
   
   return &myDesired;
 }
 
-int main(int argc, char**argv){
+int main(int argc, char**argv) {
 
-	//-1 escolhido para representar a posição do robo
-	//tamanho do robo é 500mm, ent de cada posicao da matriz pra outra
-	//é 510mm
-	//fazer modulo (%510)
+  cin >> inicial_x >> inicial_y >> head;
+  cin >> final_x >> final_y;
+
+
+  for(int i = 0; i < TAM_MATRIZ; i++) {
+    for(int j = 0; j  < TAM_MATRIZ; j++) {
+      matriz[i][j] = 1;
+    }
+  }
+
+  //-1 escolhido para representar a posição do robo
+  //tamanho do robo é 500mm, ent de cada posicao da matriz pra outra
+  //é 400mm
+  //fazer modulo (%400)
 
   Aria::init();
   ArArgumentParser parser(&argc, argv);
